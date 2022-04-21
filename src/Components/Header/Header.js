2e498018edb91d8630bafab106ff8888ae4f59f1
Kirtan Modi <kirtanmodi@192.168.10.4>
2022-04-21 13:54:07 +0530

    working 1 commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function Headers() {
   return (
     <div className="app-header">
-      <h2 className="user">The Commoner's Cafe</h2>
+      <h2 className="user">The Commoners's Kitchen</h2>
       <Logo id="header-logo"></Logo>
     </div>
   );
